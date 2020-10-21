6ad65cf324ce629a00a4a910d04a1a0e8f3da0f7
Rasha Zayed <zayed.rasha@outlook.com>
2020-10-20 21:24:07 -0700

    fixing the error message

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -6,22 +6,29 @@ const Input = (props) => {
   const { state, setState } = props;
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [data, setData] = useState({});
-  const [errorText, setErrorText] = useState({});
+  const [errorText, setErrorText] = useState(false);
+
+  console.log(errorText)
 
   useEffect(() => {
+    if(isSubmitted){
+
     axios
       .get(apiurl + state.category + "/" + state.id + "/")
 
       .then((response) => {
-        setData({ ...data, ...response.data });
+        setData({...response.data });
       })
-      // .then (response => console.log(response.data))
+      
 
-      .catch((reject) => console.log("error"));
+      .catch((reject) => setErrorText(true));
+     
+    }
   }, [isSubmitted]);
   console.log(data);
+  console.log(errorText)
 
-  const onClickHandler = (e) => {
+  const onClickHandler = (e) => { //set the change value in search field as a state.category
     e.preventDefault();
 
     setState({
@@ -29,20 +36,77 @@ const Input = (props) => {
       category: e.target.value,
     });
   };
+  
 
-  const idHandler = (e) => {
+  const idHandler = (e) => {    //set the change in id field as a state.id
+    console.log(state);
     e.preventDefault();
     setState({
       ...state,
       id: e.target.value,
     });
-    console.log(state.id);
+  
+
   };
 
   const startSearching = (e) => {
     e.preventDefault();
     setIsSubmitted(true);
   };
+  var displayResults;
+  
+
+ if (state.category == {} ){
+      displayResults = <div></div>
+  } else if (state.category == 'people' && isSubmitted== true) {
+      displayResults = 
+      <div>
+      <h1>{data.name}</h1>
+      <p>
+        <span>Height:</span> {data.height}
+      </p>
+      <p>
+        <span>Hair Color:</span> {data.hair_color}
+      </p>
+      <p>
+        <span>Eye Color: </span>
+        {data.eye_color}
+      </p>
+      <p>
+        <span>Skin Color:</span> {data.skin_color}
+      </p>
+    </div>
+  } else if (state.category =='planets' && isSubmitted== true) {
+     
+      displayResults = 
+      <div>
+      <h1>{data.name}</h1>
+      <p>
+        <span>Climate:</span> {data.climate}
+      </p>
+      <p>
+        <span>Terrain:</span> {data.terrain}
+      </p>
+      <p>
+        <span>Surface Water: </span>
+        {data.surface_water}
+      </p>
+      <p>
+        <span>Population:</span> {data.population}
+      </p>
+    </div>
+  
+  }
+  var displayErrorText;
+  if (errorText ==false){
+    displayErrorText = <div></div>;
+} else if (errorText == true && isSubmitted==true){
+    displayErrorText = <div className='error'>Ooops, You might entered wrong name or id please make sure the name is people or planets, and the id is a number!</div>
+    
+};
+
+
+
 
   return (
     <div>
@@ -63,41 +127,14 @@ const Input = (props) => {
         />
       </form>
       <div className="display">
-        {state.category == "people" ? (
-          <div>
-            <h1>{data.name}</h1>
-            <p>
-              <span>Height:</span> {data.height}
-            </p>
-            <p>
-              <span>Hair Color:</span> {data.hair_color}
-            </p>
-            <p>
-              <span>Eye Color: </span>
-              {data.eye_color}
-            </p>
-            <p>
-              <span>Skin Color:</span> {data.skin_color}
-            </p>
-          </div>
-        ) : (
-          <div>
-            <h1>{data.name}</h1>
-            <p>
-              <span>Climate:</span> {data.climate}
-            </p>
-            <p>
-              <span>Terrain:</span> {data.terrain}
-            </p>
-            <p>
-              <span>Surface Water: </span>
-              {data.surface_water}
-            </p>
-            <p>
-              <span>Population:</span> {data.population}
-            </p>
-          </div>
-        )}
+
+       
+      
+       {displayResults}
+       
+       {displayErrorText}
+       
+
       </div>
     </div>
   );
